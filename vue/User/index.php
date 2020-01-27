4950dd11e10b0131734c745a25710fac79e6243c
kozanami <gj.promail@gmail.com>
2020-01-27 19:00:56 +0100

    version:2.1| Ajout de la vue profil

--- vue/User/index.php
+++ vue/User/index.php
@@ -2,15 +2,33 @@
 	if (isset($user)) 
 	{
 ?>
-		<div class="card" style="width: 18rem;">
-		<img src="<?php WEBROOT.'img/'.$user->getAvatar() ?>" alt="..." class="card-img-top">
-		<div class="card-body">
-			<h5 class="card-title">Mon Profil</h5>
-			<li> Nom : <?php $user->getLastName(); ?></li>
-			<?php $user->getFirstName(); ?>
-			<?php $user->getEmail(); ?>
+	<div class="d-flex justify-content-center pb-2 pt-2">
+		<div class="container-fluid col-md-4 bg-dark pb-3 pt-3">
+			<form action="">
+				<li class="list-group-item bg-dark">
+					<img src="<?php echo WEBROOT.'img/avatar/'.$user->getAvatar(); ?>" alt="image de profil" class="d-flex img-thumbnail bg-dark border-info mb-3 mx-auto"><input type="file" class="btn btn-info btn-lg btn-block" value="Editer mon Avatar">
+				</li>
+				<table class="table table-striped table-dark mb-0">
+					<td>
+						<ul class="list-group">
+						<li class="list-group-item bg-dark"> Email : <?php echo $user->getEmail(); ?></li>
+							<li class="list-group-item bg-dark"> <input type="text" class="form-control bg-dark text-light" value="<?php echo $user->getEmail(); ?>"></li>
+							<li class="list-group-item bg-dark"> Nom : <?php echo $user->getLastName(); ?></li>
+							<li class="list-group-item bg-dark"> <input type="text" class="form-control bg-dark text-light" value="<?php echo $user->getLastName(); ?>"></li>
+							<li class="list-group-item bg-dark"> Prénom : <?php echo $user->getFirstName(); ?></li>
+							<li class="list-group-item bg-dark"> <input type="text" class="form-control bg-dark text-light" value="<?php echo $user->getFirstName(); ?>"></li>
+							<li class="list-group-item bg-dark"> <input type="submit" class="btn btn-success btn-lg btn-block" value="Editer mon profil"> </li>
+							<li class="list-group-item bg-dark"> <input type="submit" class="btn btn-danger btn-lg btn-block" value="Supprimer mon compte"> </li>
+						</ul>
+					</td>
+				</table>
+			</form>
 		</div>
-		</div>
+	</div>
 <?php 
+	}
+	else
+	{
+
 	}
 ?>
